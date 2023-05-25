e08602310908c650168d1520dccabc9b8603d033
Lisa Parma <lisa.parma@zextras.com>
2023-05-25 17:59:11 +0200

    feat: income meeting notification

--- src/meetings/components/MeetingNotification.tsx
+++ src/meetings/components/MeetingNotification.tsx
@@ -4,16 +4,107 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { Container } from '@zextras/carbonio-design-system';
-import React, { ReactElement } from 'react';
+import {
+	Avatar,
+	Button,
+	Container,
+	IconButton,
+	Input,
+	Text
+} from '@zextras/carbonio-design-system';
+import { size } from 'lodash';
+import React, { ReactElement, useCallback, useMemo, useState } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import styled from 'styled-components';
 
+import { UsersApi } from '../../network';
+import { getXmppClient } from '../../store/selectors/ConnectionSelector';
+import { getUserName } from '../../store/selectors/UsersSelectors';
+import useStore from '../../store/Store';
 import { MeetingCreatedEvent } from '../../types/network/websocket/wsMeetingEvents';
 
 type MeetingNotificationProps = {
 	event: MeetingCreatedEvent;
+	removeNotification: (notificationId: string) => void;
 };
-const MeetingNotification = ({ event }: MeetingNotificationProps): ReactElement => (
-	<Container> {event.id}</Container>
-);
+
+const NotificationContainer = styled(Container)`
+	box-shadow: 0px 0px 4px rgba(166, 166, 166, 0.5);
+	border-radius: 4px;
+`;
+const MeetingNotification = ({
+	event,
+	removeNotification
+}: MeetingNotificationProps): ReactElement => {
+	const xmppClient = useStore(getXmppClient);
+	const userName: string = useStore((store) => getUserName(store, event.from)) || '';
+
+	const [t] = useTranslation();
+	const userIsInvitingYouLabel = (
+		<Trans
+			i18nKey="meeting.newMeetingNotification.userIsInvitingYou"
+			defaults="<strong>{{userName}}</strong> is inviting you"
+			values={{ name: userName }}
+		/>
+	);
+	const sendAQuickMessageLabel = t(
+		'meeting.newMeetingNotification.sendAQuickMessage',
+		'Send a quick message'
+	);
+	const declineLabel = t('action.decline', 'Decline');
+	const joinMeetingLabel = t('action.joinMeeting', 'Join meeting');
+
+	const [message, setMessage] = useState('');
+
+	const onTextChange = useCallback((e) => setMessage(e.currentTarget.value.trim()), []);
+
+	const sendMessage = useCallback(() => {
+		xmppClient.sendChatMessage(event.roomId, message);
+		setMessage('');
+	}, [event.roomId, message, xmppClient]);
+
+	const disableSendMessage = useMemo(() => size(message) === 0, [message]);
+
+	const declineMeeting = useCallback(
+		() => removeNotification(event.id),
+		[event.id, removeNotification]
+	);
+
+	const joinMeeting = useCallback(() => {
+		window.open(`external/${event.roomId}`);
+		removeNotification(event.id);
+	}, [event, removeNotification]);
+
+	const picture = useMemo(() => UsersApi.getURLUserPicture(event.from), [event.from]);
+
+	return (
+		<NotificationContainer width="fill" height="fill" background="gray6" padding="1rem" gap="1rem">
+			<Container>
+				<Avatar size="large" label={userName} title={userName} picture={picture} />
+				<Text>{userIsInvitingYouLabel}</Text>
+			</Container>
+			<Container orientation="horizontal" gap="0.5rem">
+				<Input
+					value={message}
+					label={sendAQuickMessageLabel}
+					onEnter={sendMessage}
+					onChange={onTextChange}
+				/>
+				<IconButton
+					icon="Navigation2"
+					size="extralarge"
+					iconColor="primary"
+					type="outlined"
+					disabled={disableSendMessage}
+					onClick={sendMessage}
+				/>
+			</Container>
+			<Container orientation="horizontal" gap="0.5rem">
+				<Button label={declineLabel} color="secondary" onClick={declineMeeting} />
+				<Button label={joinMeetingLabel} onClick={joinMeeting} />
+			</Container>
+		</NotificationContainer>
+	);
+};
 
 export default MeetingNotification;
--- src/meetings/components/MeetingNotificationsHandler.tsx
+++ src/meetings/components/MeetingNotificationsHandler.tsx
@@ -4,29 +4,74 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { Container } from '@zextras/carbonio-design-system';
-import { map } from 'lodash';
+import { Button, Container, Portal } from '@zextras/carbonio-design-system';
+import { map, remove, size } from 'lodash';
 import React, { ReactElement, useCallback, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
 
 import MeetingNotification from './MeetingNotification';
 import useEventListener, { EventName } from '../../hooks/useEventListener';
 import { MeetingCreatedEvent } from '../../types/network/websocket/wsMeetingEvents';
 
+const PortalContainer = styled(Container)`
+	position: fixed;
+	z-index: 9999;
+	top: 0;
+	right: 0;
+`;
+
 const MeetingNotificationsHandler = (): ReactElement => {
+	const [t] = useTranslation();
+	const declineAllLabel = t('action.declineAll', 'Decline all');
+
 	const [notificationArray, setNotificationArray] = useState<MeetingCreatedEvent[]>([]);
 
-	const addIncomingMeeting = useCallback(({ detail: meetingCreatedEvent }) => {
-		setNotificationArray((prev) => [...prev, meetingCreatedEvent]);
-	}, []);
-
-	useEventListener(EventName.INCOMING_MEETING, addIncomingMeeting);
-
-	const notificationComponents = useMemo(
-		() => map(notificationArray, (notification) => <MeetingNotification event={notification} />),
-		[notificationArray]
+	const addNotification = useCallback(
+		({ detail: meetingCreatedEvent }) =>
+			setNotificationArray((prev) => [...prev, meetingCreatedEvent]),
+		[]
 	);
 
-	return <Container> {notificationComponents} </Container>;
+	const removeNotification = useCallback(
+		(notificationId: string): void =>
+			setNotificationArray((prev: MeetingCreatedEvent[]) => {
+				const notificationArray = [...prev];
+				remove(notificationArray, (notification) => notification.id === notificationId);
+				return notificationArray;
+			}),
+		[]
+	);
+
+	useEventListener(EventName.INCOMING_MEETING, addNotification);
+
+	const notificationComponents = useMemo(
+		() =>
+			map(notificationArray, (notification) => (
+				<MeetingNotification event={notification} removeNotification={removeNotification} />
+			)),
+		[notificationArray, removeNotification]
+	);
+
+	const declineAll = useCallback(() => setNotificationArray([]), []);
+
+	const displayPortal = useMemo(() => size(notificationArray) > 0, [notificationArray]);
+
+	return (
+		<Portal show={displayPortal}>
+			<PortalContainer
+				width="fit"
+				height="fit"
+				padding={{ top: '4.75rem', right: '1rem' }}
+				gap="1rem"
+			>
+				{size(notificationArray) > 1 && (
+					<Button color="secondary" label={declineAllLabel} onClick={declineAll} width="fill" />
+				)}
+				{notificationComponents}
+			</PortalContainer>
+		</Portal>
+	);
 };
 
 export default MeetingNotificationsHandler;
