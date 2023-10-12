9986adf728fbb2d0eb8ee2da561c3d568e1c5197
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-10-12 11:00:34 +0200

    refactor: remove ds augmentation and improve styled-components DefaultTheme overload
    refs: WSC-1100

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -3,56 +3,10 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-
-export interface ThemeObj {
-	windowObj: Window;
-	breakpoints: {
-		width: number;
-		aspectRatio: number;
-	};
-	borderRadius: string;
-	fonts: {
-		default: string;
-		weight: { light: number; regular: number; medium: number; bold: number };
-	};
-	sizes: {
-		font: ThemeSizeObjExtended;
-		icon: ThemeSizeObj;
-		avatar: Omit<ThemeSizeObjExtended<{ diameter: string; font: string }>, 'extrasmall'>;
-		padding: ThemeSizeObjExtended;
-	};
-	icons: Record<string, IconComponent>;
-	loginBackground: string;
-	logo: {
-		svg: IconComponent;
-		size: ThemeSizeObj;
-	};
-	palette: Record<
-		| 'currentColor'
-		| 'transparent'
-		| 'primary'
-		| 'secondary'
-		| 'header'
-		| 'highlight'
-		| 'gray0'
-		| 'gray1'
-		| 'gray2'
-		| 'gray3'
-		| 'gray4'
-		| 'gray5'
-		| 'gray6'
-		| 'warning'
-		| 'error'
-		| 'success'
-		| 'info'
-		| 'text',
-		ThemeColorObj
-	>;
-	avatarColors: Record<`avatar_${number}`, string>;
-}
+import { type Theme } from '@zextras/carbonio-design-system';
 
 declare module 'styled-components' {
 	// Augment DefaultTheme as suggested inside styled-components module
 	// eslint-disable-next-line @typescript-eslint/no-empty-interface
-	export interface DefaultTheme extends ThemeObj {}
+	export interface DefaultTheme extends Theme {}
 }
