2d9b2d00849d92c06792f3a143af6c4a8817340f
Lisa Parma <lisa.parma@zextras.com>
2024-01-12 10:53:02 +0100

    fix: pr comments

--- src/utils/calcReads.ts
+++ src/utils/calcReads.ts
@@ -13,16 +13,17 @@ import { Message, TextMessage } from '../types/store/MessageTypes';
 import { Member } from '../types/store/RoomTypes';
 
 export function calcReads(messageDate: number, roomId: string): MarkerStatus {
-	const roomMessages: Message[] = useStore.getState().messages[roomId];
-	const roomMarkers: RoomMarkers = useStore.getState().markers[roomId];
-	const members: Member[] | undefined = useStore.getState().rooms[roomId]?.members || [];
+	const store = useStore.getState();
+	const roomMessages: Message[] = store.messages[roomId];
+	const roomMarkers: RoomMarkers = store.markers[roomId];
+	const members: Member[] | undefined = store.rooms[roomId]?.members || [];
+	const sessionId = store.session.id;
 
 	let read: MarkerStatus = MarkerStatus.UNREAD;
 
 	if (!!roomMarkers && size(members) > 0) {
 		const readBy: string[] = [];
 		forEach(roomMarkers, (marker: Marker, userId: string) => {
-			const sessionId = useStore.getState().session.id;
 			const markedMessage = find(
 				roomMessages,
 				(message: Message) => message.id === marker.messageId
