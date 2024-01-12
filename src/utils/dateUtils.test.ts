5be9fc95e197e61d5d3c7a0d4c9d1161ac734fae
Lisa Parma <lisa.parma@zextras.com>
2024-01-12 10:02:11 +0100

    test: add test for dateUtils

--- src/utils/dateUtils.test.ts
+++ src/utils/dateUtils.test.ts
@@ -4,22 +4,19 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import { now } from 'moment';
-import moment from 'moment-timezone';
 
 import {
 	dateString,
 	dateToISODate,
 	dateToTimestamp,
 	formatDate,
+	now,
 	setDateDefault
 } from './dateUtils';
-import useStore from '../store/Store';
 
 beforeEach(() => {
 	setDateDefault('Europe/Rome', 'it');
 });
-
 describe('date utils', () => {
 	describe('formatDate', () => {
 		test('Timestamp is converted in a string with "YYYY-MM-DD" format', () => {
@@ -72,68 +69,53 @@ describe('date utils', () => {
 	});
 
 	describe('dateString', () => {
-		const oneHour = 1000 * 60 * 60;
+		const oneMinute = 1000 * 60;
+		const oneHour = oneMinute * 60;
+		const oneDay = oneHour * 24;
 
-		test('1 hour ago', () => {
-			const actualDate = now();
-			const actualHourMoreOne = actualDate - oneHour;
-			expect(dateString(actualHourMoreOne)).toBe('Today');
+		test('1 hour ago is today', () => {
+			const differenceDate = now() - oneHour;
+			expect(dateString(differenceDate)).toBe('Today');
 		});
 
-		test('30 hours ago', () => {
-			const actualDate = now();
-			const actualDayMoreTwo = actualDate - oneHour * 30;
-			expect(dateString(actualDayMoreTwo)).toBe('Yesterday');
+		test('Almost 1 day ago is yesterday', () => {
+			const differenceDate = now() - oneDay - oneMinute * 2;
+			expect(dateString(differenceDate)).toBe('Yesterday');
 		});
-	});
 
-	test('given two dates, the function will return the right string', () => {
-		const store = useStore.getState();
-		const timezone = store.session.userPrefTimeZone;
+		test('30 hours ago is yesterday', () => {
+			const differenceDate = now() - oneDay - oneHour * 6;
+			expect(dateString(differenceDate)).toBe('Yesterday');
+		});
 
-		// actual date
-		// should return today
-		const actualDate = moment.tz();
-		const todayString = dateString(+actualDate);
-		expect(todayString).toBe('Today');
+		test('3 days, 23 hours, 59 min, 50 sec ago is a day of a week', () => {
+			const differenceDate = now() - oneDay * 3 - oneHour * 23 - oneMinute * 59 - 50000;
+			expect(dateString(differenceDate)).toMatch(
+				/(lunedì|martedì|mercoledì|giovedì|venerdì|sabato|domenica)/
+			);
+		});
 
-		// date minus almost 1 day
-		// should return yesterday
-		const minusAlmost1Date = +actualDate - 90000000;
-		const almostYesterdayString = dateString(+minusAlmost1Date);
-		expect(almostYesterdayString).toBe('Yesterday');
+		test('4 days ago is a day of a week', () => {
+			const differenceDate = now() - oneDay * 4;
+			expect(dateString(differenceDate)).toMatch(
+				/(lunedì|martedì|mercoledì|giovedì|venerdì|sabato|domenica)/
+			);
+		});
 
-		// date minus 1 day
-		// should return yesterday
-		const minus1Date = +actualDate - 86400000;
-		const yesterdayString = dateString(+minus1Date);
-		expect(yesterdayString).toBe('Yesterday');
+		test('7 days ago is a complete date', () => {
+			const differenceDate = now() - oneDay * 7;
+			expect(dateString(differenceDate)).toMatch(
+				// eslint-disable-next-line max-len
+				/([gennaio|febbraio|marzo|aprile|maggio|giugno|luglio|agosto|settembre|ottobre|novembre|dicembre])\s\d{1,2}\s\d{4}/
+			);
+		});
 
-		// date minus 3 days, 23 hours, 59 min, 50 sec
-		// should return a day of a week
-		const minusAlmost4Date = +actualDate - 345599000;
-		const almost4DaysString = dateString(+minusAlmost4Date);
-		expect(almost4DaysString).toBe(moment.tz(minusAlmost4Date, timezone).format('dddd'));
-
-		// date minus 4 days
-		// should return a day of the week
-		const minus4Date = +actualDate - 345600000;
-		const fourDaysString = dateString(+minus4Date);
-		expect(fourDaysString).toBe(moment.tz(minus4Date, timezone).format('dddd'));
-
-		// to check if the date are the same
-		expect(almost4DaysString).toBe(fourDaysString);
-
-		// date minus 7 days
-		// should return a complete date
-		const minus7Date = +actualDate - 604800000;
-		const sevenDaysString = dateString(+minus7Date);
-		expect(sevenDaysString).toBe(moment.tz(minus7Date, timezone).format('MMMM D YYYY'));
-
-		// date minus one year
-		// should return a complete date
-		const oneYearBefore = +actualDate - 31536000000;
-		const oneYearString = dateString(+oneYearBefore);
-		expect(oneYearString).toBe(moment.tz(oneYearBefore, timezone).format('MMMM D YYYY'));
+		test('1 year ago is a complete date', () => {
+			const differenceDate = now() - oneDay * 365;
+			expect(dateString(differenceDate)).toMatch(
+				// eslint-disable-next-line max-len
+				/([gennaio|febbraio|marzo|aprile|maggio|giugno|luglio|agosto|settembre|ottobre|novembre|dicembre])\s\d{1,2}\s\d{4}/
+			);
+		});
 	});
 });
