154c43114297a4582acf3cff08a4e80183cb3422
Dennis <dennis.ceron@zextras.com>
2024-03-01 12:23:34 +0100

    fix: cancel the debounce when user stops writing refs: WSC-1302

--- src/network/xmpp/handlers/composingMessageHandler.test.ts
+++ src/network/xmpp/handlers/composingMessageHandler.test.ts
@@ -10,13 +10,14 @@ import { createMockRoom, createMockUser } from '../../../tests/createMock';
 import { xmppClient } from '../../../tests/mockedXmppClient';
 import { pauseWritingMessage, startWritingMessage } from '../xmppMessageExamples';
 
-const composingStanza = (roomId: string, from: string): Element => {
+export const composingStanza = (roomId: string, from: string): Element => {
 	const parser = new DOMParser();
 	const messageStanza = startWritingMessage.replace('roomId', roomId).replace('userId', from);
 	const xmlToParse = parser.parseFromString(messageStanza, 'application/xml');
 	return xmlToParse.getElementsByTagName('message')[0];
 };
-const pausedStanza = (roomId: string, from: string): Element => {
+
+export const pausedStanza = (roomId: string, from: string): Element => {
 	const parser = new DOMParser();
 	const messageStanza = pauseWritingMessage.replace('roomId', roomId).replace('userId', from);
 	const xmlToParse = parser.parseFromString(messageStanza, 'application/xml');
