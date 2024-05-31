ffd1b7cfde970b6d0193e3ebabf25c70018205ca
Dennis <dennis.ceron@zextras.com>
2024-05-31 15:29:40 +0200

    test: add infopage test

---
+++ src/meetings/views/InfoPage.test.tsx
@@ -0,0 +1,63 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+
+import InfoPage from './InfoPage';
+import { useParams } from '../../../__mocks__/react-router';
+import { PAGE_INFO_TYPE } from '../../hooks/useRouting';
+import { setup } from '../../tests/test-utils';
+
+const pages = [
+	[PAGE_INFO_TYPE.ROOM_EMPTY, 'This Room is empty', 'Try later', 'It seems nobody is in this Room'],
+	[
+		PAGE_INFO_TYPE.ALREADY_ACTIVE_MEETING_SESSION,
+		'This meeting is already open in another window',
+		'Continue the meeting in the new window',
+		'There cannot be more than one active session of the same meeting'
+	],
+	[
+		PAGE_INFO_TYPE.MEETING_NOT_FOUND,
+		'The meeting you are looking for does not exist',
+		'Try later',
+		'Please check the meeting link and try again'
+	],
+	[
+		PAGE_INFO_TYPE.HANG_UP_PAGE,
+		'You left the waiting room',
+		'Maybe next time',
+		'We look forward to seeing you participate in future meetings'
+	],
+	[
+		PAGE_INFO_TYPE.NEXT_TIME_PAGE,
+		'Your access has been refused',
+		'Maybe next time',
+		'The moderators have decided to deny your access to the meeting'
+	],
+	[
+		PAGE_INFO_TYPE.MEETING_ENDED,
+		'Meeting Ended',
+		'Thanks for participating',
+		"Keep in touch with your colleagues or join your groups' meeting rooms"
+	],
+	[
+		PAGE_INFO_TYPE.UNAUTHENTICATED,
+		'You are not authenticated',
+		'login to access the meeting',
+		'You cannot join the meeting if you are not authenticated with your account'
+	]
+];
+
+describe('Info page', () => {
+	test.each(pages)('Display %s info page', async (type, title, central, desc) => {
+		useParams.mockReturnValueOnce({ infoType: type });
+		setup(<InfoPage />);
+		expect(await screen.findByText(title)).toBeVisible();
+		expect(await screen.findByText(central)).toBeVisible();
+		expect(await screen.findByText(desc)).toBeVisible();
+	});
+});
