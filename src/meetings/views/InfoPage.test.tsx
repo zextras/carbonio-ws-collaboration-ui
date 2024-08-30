927397740cd5ca0f7edf3262b11a63217adbc1be
Lisa Parma <lisa.parma@zextras.com>
2024-08-30 16:39:00 +0200

    refactor: change infoPage layout

--- src/meetings/views/InfoPage.test.tsx
+++ src/meetings/views/InfoPage.test.tsx
@@ -58,9 +58,9 @@ const pagesToCheckGuest = [
 	],
 	[
 		PAGE_INFO_TYPE.INVALID_WAITING_ROOM,
-		'Invalid waiting room',
-		'Try later',
-		'The waiting room is not valid'
+		'There are no moderators in the meeting',
+		'Try again later',
+		'You must be approved by a moderator to join this meeting.'
 	]
 ];
 
@@ -71,9 +71,9 @@ describe('Info page', () => {
 			useParams.mockReturnValueOnce({ infoType: type });
 			setup(<InfoPage />);
 
-			expect(await screen.findByText(title)).toBeVisible();
-			expect(await screen.findByText(central)).toBeVisible();
-			expect(await screen.findByText(desc)).toBeVisible();
+			expect(screen.getAllByText(new RegExp(title, 'i'))[0]).toBeVisible();
+			expect(screen.getByText(new RegExp(central, 'i'))).toBeVisible();
+			expect(screen.getByText(new RegExp(desc, 'i'))).toBeVisible();
 		}
 	);
 	test.each(pagesToCheckGuest)('Display %s info page, user is guest', async (type) => {
--- src/meetings/views/InfoPage.tsx
+++ src/meetings/views/InfoPage.tsx
@@ -5,42 +5,47 @@
  */
 import React, { ReactElement, useEffect } from 'react';
 
-import { Container, Padding, Text } from '@zextras/carbonio-design-system';
+import { Container, Text } from '@zextras/carbonio-design-system';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
+import useDarkReader from '../../hooks/useDarkReader';
 import { MeetingRoutesParams, PAGE_INFO_TYPE } from '../../hooks/useRouting';
 import { getIsLoggedUserExternal } from '../../store/selectors/SessionSelectors';
 import useStore from '../../store/Store';
 import { BrowserUtils } from '../../utils/BrowserUtils';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import sloganGif from '../assets/infoPageSlogan.gif';
 
 const CustomContainer = styled(Container)`
-	z-index: 10;
-	position: fixed;
-	height: 100%;
-	width: 100%;
-	top: 0;
+	background: linear-gradient(287deg, #0f0f34 0%, #2c4579 74.63%);
 `;
 
-const MainTextWrapper = styled(Container)`
-	color: ${({ theme }): string => theme.palette.primary.regular};
-	font-size: 2.25rem;
-	margin-top: 1.875rem;
-	font-weight: bold;
+const BoxText = styled(Container)`
+	border-radius: 1.25rem;
+	box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
 `;
 
-const ErrorWrapper = styled(Container)`
-	margin: 3rem 0;
+const Title = styled(Text)`
+	font-size: 1.75rem;
+	color: #93b2f4;
 `;
 
-const ErrorCode = styled(Container)`
-	color: #4d4d4d;
-	font-weight: 300;
-	font-size: 0.875rem;
-	line-height: 1.3125rem;
-	padding: 0 3.125rem;
-	text-transform: uppercase;
+const Description = styled(Text)`
+	font-size: 1.125rem;
+`;
+
+const Slogan = styled(Text)`
+	font-size: 1.375rem;
+`;
+
+const InfoPageGif = styled.img<{ src: string }>`
+	width: 3rem;
+	border-radius: 50%;
+	transform: scaleX(-1);
+	box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
 `;
 
 const InfoPage = (): ReactElement => {
@@ -48,8 +53,8 @@ const InfoPage = (): ReactElement => {
 
 	const [t] = useTranslation();
 	let titleLabel;
-	let centralLabel;
-	let descriptionLowerLabel;
+	let sloganLabel;
+	let descriptionLabel;
 
 	const isLoggedUserExternal = useStore(getIsLoggedUserExternal);
 
@@ -62,51 +67,51 @@ const InfoPage = (): ReactElement => {
 	switch (infoType) {
 		case PAGE_INFO_TYPE.ROOM_EMPTY:
 			titleLabel = t('external.roomIsEmpty', 'This Room is empty');
-			centralLabel = t('tryLater', 'Try later');
-			descriptionLowerLabel = t('external.nobodyInTheRoom', 'It seems nobody is in this Room');
+			sloganLabel = t('tryLater', 'Try later');
+			descriptionLabel = t('external.nobodyInTheRoom', 'It seems nobody is in this Room');
 			break;
 		case PAGE_INFO_TYPE.ALREADY_ACTIVE_MEETING_SESSION:
 			titleLabel = t(
 				'meeting.openedInAnotherWindow',
 				'This meeting is already open in another window'
 			);
-			centralLabel = t('meeting.continueActiveSession', 'Continue the meeting in the new window');
-			descriptionLowerLabel = t(
+			sloganLabel = t('meeting.continueActiveSession', 'Continue the meeting in the new window');
+			descriptionLabel = t(
 				'meeting.noMoreActiveSession',
 				'There cannot be more than one active session of the same meeting'
 			);
 			break;
 		case PAGE_INFO_TYPE.HANG_UP_PAGE:
 			titleLabel = t('meeting.infoPage.title.hangUp', 'You left the waiting room');
-			centralLabel = t('meeting.infoPage.slogan.hangUp', 'Maybe next time');
-			descriptionLowerLabel = t(
+			sloganLabel = t('meeting.infoPage.slogan.hangUp', 'Maybe next time');
+			descriptionLabel = t(
 				'meeting.infoPage.description.hangUp',
 				'We look forward to seeing you participate in future meetings'
 			);
 			break;
 		case PAGE_INFO_TYPE.NEXT_TIME_PAGE:
 			titleLabel = t('meeting.infoPage.title.rejected', 'Your access has been refused');
-			centralLabel = t('meeting.infoPage.slogan.rejected', 'Maybe next time');
-			descriptionLowerLabel = t(
+			sloganLabel = t('meeting.infoPage.slogan.rejected', 'Maybe next time');
+			descriptionLabel = t(
 				'meeting.infoPage.description.rejected',
 				'The moderators have decided to deny your access to the meeting'
 			);
 			break;
 		case PAGE_INFO_TYPE.MEETING_ENDED:
 			titleLabel = t('notification.meeting.ended', 'Meeting Ended');
-			centralLabel = t('thanksForParticipating', 'Thanks for participating');
-			descriptionLowerLabel = t(
+			sloganLabel = t('thanksForParticipating', 'Thanks for participating');
+			descriptionLabel = t(
 				'keepInTouchWithColleagues',
 				"Keep in touch with your colleagues or join your groups' meeting rooms"
 			);
 			break;
 		case PAGE_INFO_TYPE.UNAUTHENTICATED:
 			titleLabel = t('meeting.infoPage.title.notAuthenticatedUser', 'You are not authenticated');
-			centralLabel = t(
+			sloganLabel = t(
 				'meeting.infoPage.slogan.notAuthenticatedUser',
 				'login to access the meeting'
 			);
-			descriptionLowerLabel = t(
+			descriptionLabel = t(
 				'meeting.infoPage.description.notAuthenticatedUser',
 				'You cannot join the meeting if you are not authenticated with your account'
 			);
@@ -116,8 +121,8 @@ const InfoPage = (): ReactElement => {
 				'meeting.infoPage.title.invalidWaiting',
 				'There are no moderators in the meeting'
 			);
-			centralLabel = t('meeting.infoPage.slogan.invalidWaiting', 'Try again later');
-			descriptionLowerLabel = t(
+			sloganLabel = t('meeting.infoPage.slogan.invalidWaiting', 'Try again later');
+			descriptionLabel = t(
 				'meeting.infoPage.description.invalidWaiting',
 				'You must be approved by a moderator to join this meeting.'
 			);
@@ -128,8 +133,8 @@ const InfoPage = (): ReactElement => {
 				'meeting.infoPage.title.meetingNotFound',
 				'The meeting you are looking for does not exist'
 			);
-			centralLabel = t('meeting.infoPage.slogan.meetingNotFound', 'Try later');
-			descriptionLowerLabel = t(
+			sloganLabel = t('meeting.infoPage.slogan.meetingNotFound', 'Try later');
+			descriptionLabel = t(
 				'meeting.infoPage.description.meetingNotFound',
 				'Please check the meeting link and try again'
 			);
@@ -137,21 +142,44 @@ const InfoPage = (): ReactElement => {
 		}
 	}
 
+	const { darkReaderStatus, disableDarkReader } = useDarkReader();
+
+	useEffect(() => {
+		disableDarkReader();
+	}, [darkReaderStatus, disableDarkReader]);
+
 	return (
-		<CustomContainer>
-			<MainTextWrapper height="fit" width="fit">
-				{titleLabel}
-			</MainTextWrapper>
-			<ErrorWrapper height="fit" width="fit" orientation="horizontal">
-				<Container height="0.0625rem" width="6.25rem" background="gray1" />
-				<ErrorCode height="fit" width="fit">
-					{centralLabel}
-				</ErrorCode>
-				<Container height="0.0625rem" width="6.25rem" background="gray1" />
-			</ErrorWrapper>
-			<Padding vertical="large">
-				<Text weight="light">{descriptionLowerLabel}</Text>
-			</Padding>
+		<CustomContainer
+			mainAlignment="flex-start"
+			crossAlignment="flex-start"
+			padding={{ top: '25vh', left: '15vw' }}
+			gap="1rem"
+		>
+			<BoxText
+				height="fit"
+				width="fit"
+				padding={{ vertical: '3.125rem', horizontal: '2.5rem' }}
+				gap="1rem"
+				crossAlignment="flex-start"
+				background="text"
+			>
+				<Title>{titleLabel.toUpperCase()}</Title>
+				<Description size="extralarge" color="gray6">
+					{descriptionLabel}
+				</Description>
+			</BoxText>
+			<Container
+				height="fit"
+				width="fit"
+				orientation="horizontal"
+				padding={{ top: '1rem' }}
+				gap="1rem"
+			>
+				<InfoPageGif src={sloganGif} />
+				<Slogan weight="bold" color="gray6">
+					{sloganLabel.toUpperCase()}
+				</Slogan>
+			</Container>
 		</CustomContainer>
 	);
 };
