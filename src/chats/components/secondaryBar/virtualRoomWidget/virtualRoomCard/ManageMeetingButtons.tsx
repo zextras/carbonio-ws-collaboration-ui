d4a3a8b51aa410a02cb64d3c6772e311e7c9686e
Lisa Parma <lisa.parma@zextras.com>
2024-12-18 17:13:13 +0100

    refactor: virtual room actions dropdown

--- src/chats/components/secondaryBar/virtualRoomWidget/virtualRoomCard/ManageMeetingButtons.tsx
+++ src/chats/components/secondaryBar/virtualRoomWidget/virtualRoomCard/ManageMeetingButtons.tsx
@@ -8,6 +8,8 @@ import React, { FC, useCallback, useMemo, useState } from 'react';
 import {
 	Button,
 	CreateSnackbarFn,
+	Dropdown,
+	DropdownItem,
 	Row,
 	Tooltip,
 	useSnackbar
@@ -29,15 +31,7 @@ type ManageMeetingButtonsProps = {
 	meetingIsActive: boolean;
 };
 
-const CustomDeleteButton = styled(Button)<{ $isMyRoom: boolean | undefined }>`
-	${({ $isMyRoom }): string | undefined | false => !$isMyRoom && 'opacity: 0.5; cursor: default;'};
-	&:hover {
-		background-color: ${({ theme }): string => theme.palette.error.regular};
-		color: ${({ theme }): string => theme.palette.gray6.regular};
-	}
-`;
-
-const CustomMainButton = styled(Button)<{ $isMyRoom: boolean | undefined }>`
+const JoinRoomButton = styled(Button)<{ $isMyRoom: boolean | undefined }>`
 	${({ $isMyRoom }): string | undefined | false => !$isMyRoom && 'opacity: 1;'};
 `;
 
@@ -49,11 +43,12 @@ const ManageMeetingButtons: FC<ManageMeetingButtonsProps> = ({
 	meetingIsActive
 }) => {
 	const [t] = useTranslation();
-	const deleteVirtualRoomTooltip = t('meeting.virtual.deleteTooltip', 'Delete Virtual Room');
-	const copyVirtualRoomTooltip = t('meeting.virtual.copyTooltip', "Copy Virtual Room's link");
-	const startMeeting = t('meeting.startMeeting', 'Start meeting');
-	const joinMeeting = t('meeting.joinMeeting', 'Join meeting');
-	const rejoinMeeting = t('meeting.rejoinMeeting', 'Rejoin meeting');
+	const roomActionsTooltip = t('', 'Room actions'); // TODO: key translations
+	const copyVirtualRoomLabel = t('meeting.virtual.copyTooltip', "Copy Virtual Room's link");
+	const deleteVirtualRoomLabel = t('meeting.virtual.deleteTooltip', 'Delete Virtual Room');
+	const startMeetingLabel = t('meeting.startMeeting', 'Start meeting');
+	const joinMeetingLabel = t('meeting.joinMeeting', 'Join meeting');
+	const rejoinMeetingLabel = t('meeting.rejoinMeeting', 'Rejoin meeting');
 	const leaveRoomTooltip = t('modal.leaveRoom', 'Leave Room');
 	const copyVirtualRoomLinkSnackbar = t(
 		'meeting.virtual.copyLinkSnackbar',
@@ -67,15 +62,8 @@ const ManageMeetingButtons: FC<ManageMeetingButtonsProps> = ({
 	const { openMeeting, copyMeetingLink } = useRoomMeeting(roomId);
 
 	const leaveConversation = useCallback(() => {
-		if (sessionId) {
-			RoomsApi.deleteRoomMember(roomId, sessionId).then(() => {});
-		}
+		if (sessionId) RoomsApi.deleteRoomMember(roomId, sessionId);
 	}, [roomId, sessionId]);
-
-	const enterMeeting = useMemo(
-		() => (amIParticipating ? rejoinMeeting : joinMeeting),
-		[amIParticipating, joinMeeting, rejoinMeeting]
-	);
 
 	const handleCopyLink = useCallback(() => {
 		copyMeetingLink();
@@ -87,45 +75,54 @@ const ManageMeetingButtons: FC<ManageMeetingButtonsProps> = ({
 		});
 	}, [copyMeetingLink, createSnackbar, copyVirtualRoomLinkSnackbar]);
 
-	const enterRoomTooltip = useMemo(
-		() => (meetingIsActive ? enterMeeting : startMeeting),
-		[enterMeeting, meetingIsActive, startMeeting]
-	);
+	const enterRoomTooltip = useMemo(() => {
+		if (!meetingIsActive) return startMeetingLabel;
+		if (amIParticipating) return rejoinMeetingLabel;
+		return joinMeetingLabel;
+	}, [amIParticipating, joinMeetingLabel, meetingIsActive, rejoinMeetingLabel, startMeetingLabel]);
 
 	const handleModalOpening = useCallback(() => setShowModal((prevState) => !prevState), []);
 
+	const items = useMemo((): Array<DropdownItem> => {
+		const actions = [];
+		actions.push({
+			id: 'copyLinkAction',
+			label: copyVirtualRoomLabel,
+			onClick: handleCopyLink,
+			disabled: !(amIParticipating || isMyRoom)
+		});
+		if (isMyRoom) {
+			actions.push({
+				id: 'deleteRoomAction',
+				label: deleteVirtualRoomLabel,
+				onClick: handleModalOpening
+			});
+		}
+		return actions;
+	}, [
+		amIParticipating,
+		copyVirtualRoomLabel,
+		deleteVirtualRoomLabel,
+		handleCopyLink,
+		handleModalOpening,
+		isMyRoom
+	]);
+
 	return (
-		<>
-			<Row orientation="horizontal" gap="0.25rem">
-				{isMyRoom && (
-					<Tooltip label={deleteVirtualRoomTooltip}>
-						<CustomDeleteButton
-							type="ghost"
-							color="text"
-							icon="Trash2Outline"
-							onClick={handleModalOpening}
-							$isMyRoom={amIParticipating || isMyRoom}
-						/>
-					</Tooltip>
-				)}
-				<Tooltip label={copyVirtualRoomTooltip} disabled={!(amIParticipating || isMyRoom)}>
-					<Button
-						type="ghost"
-						color="text"
-						icon="Link2Outline"
-						onClick={handleCopyLink}
-						disabled={!(amIParticipating || isMyRoom)}
-					/>
-				</Tooltip>
-				<Tooltip label={amIParticipating || isMyRoom ? enterRoomTooltip : leaveRoomTooltip}>
-					<CustomMainButton
-						color={amIParticipating || isMyRoom ? 'primary' : 'error'}
-						icon={amIParticipating || isMyRoom ? 'Video' : 'LogOut'}
-						onClick={amIParticipating || isMyRoom ? openMeeting : leaveConversation}
-						$isMyRoom={amIParticipating || isMyRoom}
-					/>
-				</Tooltip>
-			</Row>
+		<Row orientation="horizontal" gap="0.25rem">
+			<Tooltip label={roomActionsTooltip} disablePortal>
+				<Dropdown items={items} placement="top-start" disablePortal>
+					<Button icon="MoreVertical" type="ghost" color="gray0" onClick={(): void => undefined} />
+				</Dropdown>
+			</Tooltip>
+			<Tooltip label={amIParticipating || isMyRoom ? enterRoomTooltip : leaveRoomTooltip}>
+				<JoinRoomButton
+					color={amIParticipating || isMyRoom ? 'primary' : 'error'}
+					icon={amIParticipating || isMyRoom ? 'Video' : 'LogOut'}
+					onClick={amIParticipating || isMyRoom ? openMeeting : leaveConversation}
+					$isMyRoom={amIParticipating || isMyRoom}
+				/>
+			</Tooltip>
 			<DeleteVirtualRoomModal
 				showModal={showModal}
 				setShowModal={setShowModal}
@@ -133,7 +130,7 @@ const ManageMeetingButtons: FC<ManageMeetingButtonsProps> = ({
 				modalRef={modalRef}
 				roomId={roomId}
 			/>
-		</>
+		</Row>
 	);
 };
 
