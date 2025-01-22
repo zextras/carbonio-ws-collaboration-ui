aa7da8cc602dc2a05640e8899e011a1396489a7c
Lisa Parma <lisa.parma@zextras.com>
2025-01-22 11:54:34 +0100

    feat: virtual rooms enhance active participants and other moderators ref: WSC-1810 (#463)

--- src/chats/components/secondaryBar/virtualRoomWidget/virtualRoomCard/InfoSection.tsx
+++ src/chats/components/secondaryBar/virtualRoomWidget/virtualRoomCard/InfoSection.tsx
@@ -3,10 +3,11 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { FC, useMemo } from 'react';
+import React, { FC, useMemo, useRef } from 'react';
 
-import { Avatar, Row, Shimmer, Text, Tooltip } from '@zextras/carbonio-design-system';
-import { useTranslation } from 'react-i18next';
+import { Avatar, Container, Row, Shimmer, Text } from '@zextras/carbonio-design-system';
+import { size } from 'lodash';
+import { Trans, useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
 import useAvatarUtilities from '../../../../../hooks/useAvatarUtilities';
@@ -14,6 +15,7 @@ import { getOwners } from '../../../../../store/selectors/RoomsSelectors';
 import { getUserId } from '../../../../../store/selectors/SessionSelectors';
 import { getUserName } from '../../../../../store/selectors/UsersSelectors';
 import useStore from '../../../../../store/Store';
+import UserPopoverList from '../../../userPopoverList/UserPopoverList';
 
 type InfoSectionProps = {
 	roomId: string;
@@ -34,6 +36,11 @@ const CustomShimmerAvatar = styled(Shimmer.Avatar)`
 
 const CustomRow = styled(Row)<{ $isMyRoom: boolean | undefined }>`
 	${({ $isMyRoom }): string | undefined | false => !$isMyRoom && 'opacity: 0.5; cursor: default;'};
+`;
+
+const ClickableText = styled(Text)<{ $displayList: boolean }>`
+	cursor: ${({ $displayList }): string | undefined | false =>
+		$displayList ? 'pointer' : 'default;'};
 `;
 
 const InfoSection: FC<InfoSectionProps> = ({
@@ -57,22 +64,31 @@ const InfoSection: FC<InfoSectionProps> = ({
 		'meeting.virtual.moderators.singleAdditional',
 		'and other one moderator'
 	);
+	const virtualRoomModeratorsLabel = t(
+		'meeting.virtual.moderators.widget',
+		"Virtual Room's moderators:"
+	);
 
 	const sessionId = useStore(getUserId);
 	const sessionName = useStore((store) => getUserName(store, sessionId ?? ''));
 	const moderatorsList = useStore((store) => getOwners(store, roomId));
-	const moderatorName = useStore((store) => getUserName(store, moderatorsList[0].userId));
+	const moderatorName = useStore((store) => getUserName(store, moderatorsList[0]));
 
-	const otherModeratorsLabel = t(
-		'meeting.virtual.moderators.multipleAdditional',
-		'and other {{numberOfModerators}} moderators',
-		{
-			numberOfModerators: moderatorsList.length - 1
-		}
+	const moderatorsTextRef = useRef(null);
+
+	const otherModeratorsLabel = useMemo(
+		() => (
+			<Trans
+				i18nKey="meeting.virtual.moderators.multipleAdditional"
+				defaults="and other <strong>{{numberOfModerators}} moderators</strong>."
+				values={{ numberOfModerators: moderatorsList.length - 1 }}
+			/>
+		),
+		[moderatorsList.length]
 	);
 
 	const { avatarPicture, avatarColor, isLoading } = useAvatarUtilities(
-		userIsModerator ? (sessionId ?? '') : moderatorsList[0].userId
+		userIsModerator ? (sessionId ?? '') : moderatorsList[0]
 	);
 
 	const ownerName = useMemo(
@@ -81,32 +97,18 @@ const InfoSection: FC<InfoSectionProps> = ({
 	);
 
 	const moderatorLabel = useMemo(() => {
-		if (moderatorsList.length === 1) {
-			return userIsModerator ? sessionOnlyModeratorLabel : isTheOnlyModeratorLabel;
-		}
-
-		if (moderatorsList.length === 2) {
-			return otherOneModeratorLabel;
-		}
-
-		return otherModeratorsLabel;
+		if (size(moderatorsList) > 2) return otherModeratorsLabel;
+		if (size(moderatorsList) === 2) return otherOneModeratorLabel;
+		if (userIsModerator) return sessionOnlyModeratorLabel;
+		return isTheOnlyModeratorLabel;
 	}, [
-		moderatorsList.length,
+		moderatorsList,
 		otherModeratorsLabel,
+		otherOneModeratorLabel,
 		userIsModerator,
-		isTheOnlyModeratorLabel,
 		sessionOnlyModeratorLabel,
-		otherOneModeratorLabel
+		isTheOnlyModeratorLabel
 	]);
-
-	const moderatorsNameList = useMemo(() => {
-		if (moderatorsList.length > 1) {
-			return moderatorsList
-				.map((moderator) => getUserName(useStore.getState(), moderator.userId))
-				.join(', ');
-		}
-		return '';
-	}, [moderatorsList]);
 
 	return (
 		<CustomRow
@@ -132,11 +134,26 @@ const InfoSection: FC<InfoSectionProps> = ({
 				<Text size="small" overflow="ellipsis">
 					{ownerName}
 				</Text>
-				<Tooltip label={moderatorsNameList} disabled={moderatorsList.length <= 1}>
-					<Text size="extrasmall" weight="light" color="gray1">
+				<Container ref={moderatorsTextRef} crossAlignment="flex-start" width="fit">
+					<ClickableText
+						size="extrasmall"
+						weight="light"
+						color="gray1"
+						ref={moderatorsTextRef}
+						$displayList={size(moderatorsList) > 1}
+					>
 						{moderatorLabel}
-					</Text>
-				</Tooltip>
+					</ClickableText>
+					{size(moderatorsList) > 1 && (
+						<UserPopoverList
+							anchorEl={moderatorsTextRef}
+							userList={moderatorsList}
+							title={virtualRoomModeratorsLabel}
+							icon="Crown"
+							placement="right"
+						/>
+					)}
+				</Container>
 			</Row>
 		</CustomRow>
 	);
