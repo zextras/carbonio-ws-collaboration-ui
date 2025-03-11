eaa2b9c23f68385ba52839164c26f68410ba947d
Lisa Parma <lisa.parma@zextras.com>
2025-03-11 14:09:25 +0100

    fix: wait user info response before showing browser notification

--- src/tests/mocks/network.ts
+++ src/tests/mocks/network.ts
@@ -9,6 +9,7 @@
 import attachmentsApi from '../../network/apis/AttachmentsApi';
 import meetingsApi from '../../network/apis/MeetingsApi';
 import roomsApi from '../../network/apis/RoomsApi';
+import usersApi from '../../network/apis/UsersApi';
 
 export enum RoomsApiToSpy {
 	GET_ROOM = 'getRoom',
@@ -53,6 +54,11 @@ export enum MeetingsApiToSpy {
 	CREATE_GUEST_ACCOUNT = 'createGuestAccount'
 }
 
+export enum UsersApiToSpy {
+	GET_USERS = 'getUsers',
+	GET_USER = 'getUser'
+}
+
 export const spyOnAttachmentsApi: (apiToSpy: AttachmentsApiToSpy) => jest.SpyInstance = (
 	apiToSpy: AttachmentsApiToSpy
 ) => jest.spyOn(attachmentsApi, apiToSpy);
@@ -64,3 +70,7 @@ export const spyOnRoomsApi: (apiToSpy: RoomsApiToSpy) => jest.SpyInstance = (
 export const spyOnMeetingsApi: (apiToSpy: MeetingsApiToSpy) => jest.SpyInstance = (
 	apiToSpy: MeetingsApiToSpy
 ) => jest.spyOn(meetingsApi, apiToSpy);
+
+export const spyOnUsersApi: (apiToSpy: UsersApiToSpy) => jest.SpyInstance = (
+	apiToSpy: UsersApiToSpy
+) => jest.spyOn(usersApi, apiToSpy);
