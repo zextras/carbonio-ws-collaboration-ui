eb116ee65fa01fe490245206536179bddc6eaa0f
Lisa Parma <lisa.parma@zextras.com>
2025-04-03 15:44:24 +0200

    fix: avoid calling requestHistory for an unknown room ref: WSC-1901 (#503)

--- src/network/xmpp/XMPPClient.test.ts
+++ src/network/xmpp/XMPPClient.test.ts
@@ -8,9 +8,11 @@ import { onGetLastActivityResponse } from './handlers/lastActivityHandler';
 import { onGetRosterResponse } from './handlers/rosterHandler';
 import XMPPClient from './XMPPClient';
 import { XMPPRequestType } from './XMPPConnection';
+import useStore from '../../store/Store';
 import { createMockRoom } from '../../tests/createMock';
 import { RoomsApiToSpy, spyOnRoomsApi } from '../../tests/mocks/network';
 import { pingStanza } from '../../tests/mocks/XMPPStanza';
+import { dateToTimestamp } from '../../utils/dateUtils';
 
 describe('XMPPClient', () => {
 	test('connect is called with the correct params', () => {
@@ -64,6 +66,31 @@ describe('XMPPClient', () => {
 			elem: expect.any(Object),
 			callback: onGetLastActivityResponse
 		});
+	});
+
+	test('requestHistory is called for a known room', () => {
+		const room = createMockRoom({ id: 'room-test' });
+		useStore.getState().addRoom(createMockRoom({ id: room.id }));
+		const xmppClient = new XMPPClient();
+		const spyOnXmppSend = jest.spyOn(xmppClient.xmppConnection, 'send');
+		xmppClient.requestHistory(room.id, dateToTimestamp('2024-03-12'), 10);
+		xmppClient.requestHistoryBetweenTwoMessage(room.id, 'messageId1', 'messageId2');
+		xmppClient.requestMessageSubjectOfReply(room.id, 'messageId1', 'messageId2');
+		xmppClient.requestFullHistory(room.id);
+
+		expect(spyOnXmppSend).toHaveBeenCalledTimes(4);
+	});
+
+	test('requestHistory is not called for an unknown room', () => {
+		const room = createMockRoom({ id: 'room-test' });
+		const xmppClient = new XMPPClient();
+		const spyOnXmppSend = jest.spyOn(xmppClient.xmppConnection, 'send');
+		xmppClient.requestHistory(room.id, dateToTimestamp('2024-03-12'), 10);
+		xmppClient.requestHistoryBetweenTwoMessage(room.id, 'messageId1', 'messageId2');
+		xmppClient.requestMessageSubjectOfReply(room.id, 'messageId1', 'messageId2');
+		xmppClient.requestFullHistory(room.id);
+
+		expect(spyOnXmppSend).toHaveBeenCalledTimes(0);
 	});
 
 	test('sendChatMessage should send a message', () => {
