2242d2e331d3e366c5f86b1b81b37e994d1fbcef
Lisa Parma <lisa.parma@zextras.com>
2025-07-08 14:41:21 +0200

    fix: forward messages with special characters ref: WSC-2067

--- src/network/apis/RoomsApi.ts
+++ src/network/apis/RoomsApi.ts
@@ -283,7 +283,7 @@ class RoomsApi implements IRoomsApi {
 					message.stanzaId
 				);
 				if (historyMessage) {
-					historyMessage.getElementsByTagName('body')[0].innerHTML = message.text;
+					historyMessage.getElementsByTagName('body')[0].textContent = message.text;
 					listOfMessages[message.stanzaId] = historyMessage.outerHTML;
 				}
 			})
