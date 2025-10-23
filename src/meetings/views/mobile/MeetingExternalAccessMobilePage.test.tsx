ddd029fb9809c04a16c7ad7952ec2857566d49d0
Lisa Parma <lisa.parma@zextras.com>
2025-10-23 17:55:44 +0200

    test: external page test

---
+++ src/meetings/views/mobile/MeetingExternalAccessMobilePage.test.tsx
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+
+import MeetingExternalAccessMobilePage from './MeetingExternalAccessMobilePage';
+import { MeetingsApiToSpy, spyOnMeetingsApi } from '../../../tests/mocks/network';
+import { setup } from '../../../tests/test-utils';
+
+describe('MeetingExternalAccessMobilePage tests', () => {
+	test('Meeting name is displayed correctly', async () => {
+		spyOnMeetingsApi(MeetingsApiToSpy.GET_SCHEDULED_MEETING_NAME).mockResolvedValue({
+			name: 'Test Meeting'
+		});
+		setup(<MeetingExternalAccessMobilePage />);
+
+		expect(await screen.findByText('Welcome to "Test Meeting" virtual room')).toBeInTheDocument();
+	});
+
+	test('Create new guest from mobile', async () => {
+		const spyCreateGuest = spyOnMeetingsApi(
+			MeetingsApiToSpy.CREATE_GUEST_ACCOUNT
+		).mockResolvedValueOnce({
+			id: 'user123'
+		});
+
+		const { user } = setup(<MeetingExternalAccessMobilePage />);
+		const nameInput = await screen.findByPlaceholderText('Enter your name');
+		await user.type(nameInput, 'Guest User');
+		const readyButton = await screen.findByText('Ready to participate');
+		await user.click(readyButton);
+		expect(spyCreateGuest).toHaveBeenCalled();
+	});
+});
