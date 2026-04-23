bb1b1704911dc8f9f217cc21c56b40711770f402
galvagnimatteo <galvagni.matteo@protonmail.com>
2026-04-23 09:08:10 +0200

    chore: add SPDX headers to .dockerignore and direct-build.js

--- direct-build.js
+++ direct-build.js
@@ -1,4 +1,8 @@
 /*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *
  * Direct build script to bypass SDK's yargs ESM issue
  */
 const webpack = require('webpack');
