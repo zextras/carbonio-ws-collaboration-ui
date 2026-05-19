8026fd7e52dbc10016c7841f9c08f5bc5e22ef08
Lisa Parma <lisa.parma@zextras.com>
2026-05-19 10:57:39 +0200

    feat: setup shared repo

---
+++ shared/vitest.config.ts
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		globals: true
+	},
+	cacheDir: '../node_modules/.vite'
+});
